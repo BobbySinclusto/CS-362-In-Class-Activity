add3b54ba23ba2e0bddc0ada60ad6e49c97523f2
BobbySinclusto <allencbenjamin@gmail.com>
2021-04-15 17:08:52 -0700

    finished password generator function

--- passwords.py
+++ passwords.py
@@ -2,4 +2,4 @@ import random
 
 def gen_pwd(n):
     alphabet = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM~!@#$%^&*()_+'
-
+    print(''.join([random.choice(alphabet) for i in range(n)]))
