a92649e0aad32b7f5d9cf2df7026ed307cfeaece
BobbySinclusto <allencbenjamin@gmail.com>
2021-04-15 17:09:23 -0700

    added user input for password generator

--- passwords.py
+++ passwords.py
@@ -3,3 +3,5 @@ import random
 def gen_pwd(n):
     alphabet = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM~!@#$%^&*()_+'
     print(''.join([random.choice(alphabet) for i in range(n)]))
+
+n = int(input('Enter the number of characters you want in your password: '))
